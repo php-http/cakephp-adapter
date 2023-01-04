463a6aa58310f64ede669280363feb9fb8215a8c
mscherer <mark.scherer@spryker.com>
2023-01-04 19:05:36 +0100

    Fix up CI. PHP 7.4+

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Http\Adapter\Cake\Tests;
 
+use Cake\Http\Client as CakeClient;
 use Cake\Http\Client\Response;
 use Cake\Http\ServerRequest;
 use Http\Adapter\Cake\Client;
@@ -25,17 +26,20 @@ class ClientTest extends TestCase
     {
         $serverRequest = new ServerRequest();
 
-        $client = new Client();
+        $cakeClient = $this->createClientMock();
+        $cakeClient->expects($this->once())->method('send')->willReturn(new Response());
+
+        $client = new Client($cakeClient);
         $response = $client->sendRequest($serverRequest);
 
         $this->assertInstanceOf(Response::class, $response);
     }
 
     /**
-     * @return \Http\Adapter\Cake\Client|\PHPUnit\Framework\MockObject\MockObject
+     * @return \Cake\Http\Client|\PHPUnit\Framework\MockObject\MockObject
      */
-    protected function createHttpAdapterMock(): Client
+    protected function createClientMock(): CakeClient
     {
-        return $this->getMockBuilder(Client::class)->getMock();
+        return $this->getMockBuilder(CakeClient::class)->onlyMethods(['send'])->getMock();
     }
 }
